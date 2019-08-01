f1e06803b9881557d6a99e3e1c5a31eba7b9b2b2
Tim Savage <tim@savage.company>
2019-08-02 07:46:40 +1000

    tied up errors added in support for len startwsith/endswith

--- sample.py
+++ sample.py
@@ -1,15 +1,15 @@
 import ast
+import readline
 
-MY_EVENT = {
-    "x": 123,
-    "y": "321",
-    "foo": True,
-}
+MY_EVENT = {"x": 123, "y": "321", "foo": True}
 
 
 def verify_expression(st):
-    def _validate(node):
+    """
+    Validate and verify event matching expression.
+    """
 
+    def _validate(node):
         # Allow and process compare statements
         if isinstance(node, ast.Compare):
             _validate(node.left)
@@ -19,15 +19,30 @@ def verify_expression(st):
         # Allow certain literals and constants
         elif isinstance(node, (ast.Num, ast.Str, ast.Bytes, ast.List, ast.Tuple, ast.NameConstant)):
             pass
-        
+
+        # Allow certain calls (len)
+        elif isinstance(node, ast.Call):
+            # Allow use of len
+            if isinstance(node.func, ast.Name):
+                if node.func.id not in ("len",):
+                    raise NameError(f"{node.func.id!r} not found.")
+
+            # Allow use of startswith, endswith
+            elif isinstance(node.func, ast.Attribute):
+                if node.func.attr not in ("startswith", "endswith"):
+                    raise NameError(f"{node.func.attr!r} not found.")
+
+            else:
+                raise SyntaxError(f"Unsupported node {node!r}")
+
         # Filter the available names to those in an event
         elif isinstance(node, ast.Name):
             if node.id not in MY_EVENT:
-                raise NameError(f"Unknown variable {node.id}")
-        
+                raise NameError(f"{node.id!r} not found")
+
         else:
-            raise RuntimeError(f"Unsupported node {node!r}")
-    
+            raise SyntaxError(f"Unsupported node {node!r}")
+
     _validate(st.body)
 
 
@@ -35,7 +50,7 @@ print(f"Event is: {MY_EVENT}")
 while True:
     try:
         expr = input("\nMatching expression: ")
-    except KeyboardInterrupt:
+    except (KeyboardInterrupt, EOFError):
         print()
         exit()
 
@@ -44,10 +59,10 @@ while True:
     except SyntaxError as ex:
         print(f"Invalid expression: {ex}")
         continue
-    
+
     try:
         verify_expression(st)
-    except RuntimeError as ex:
+    except (SyntaxError, NameError) as ex:
         print(str(ex))
         continue
 
@@ -58,4 +73,3 @@ while True:
     else:
         if matched:
             print("Event matched!")
-
