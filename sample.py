518da7347b9fb9c1fc04b5ef5268433d68d2c30f
Tim Savage <tim@savage.company>
2019-08-01 17:05:37 +1000

    Added more code, tweaked examples

--- sample.py
+++ sample.py
@@ -1,40 +1,40 @@
 import ast
-import sys
 
-MY_OBJ = {
+MY_EVENT = {
     "x": 123,
-    "y": 321,
+    "y": "321",
     "foo": True,
 }
 
 
 def verify_expression(st):
     def _validate(node):
+
+        # Allow and process compare statements
         if isinstance(node, ast.Compare):
             _validate(node.left)
+            # Allow all operations so don't check operators
             [_validate(c) for c in node.comparators]
 
+        # Allow certain literals and constants
         elif isinstance(node, (ast.Num, ast.Str, ast.Bytes, ast.List, ast.Tuple, ast.NameConstant)):
-            pass  # Allow all constants
+            pass
         
+        # Filter the available names to those in an event
         elif isinstance(node, ast.Name):
-            if node.id not in MY_OBJ:
+            if node.id not in MY_EVENT:
                 raise NameError(f"Unknown variable {node.id}")
         
         else:
             raise RuntimeError(f"Unsupported node {node!r}")
     
-    # Check for a comparison
-    if not isinstance(st.body, ast.Compare):
-        raise RuntimeError(f"Expected comparison got {type(expr)}")
-
     _validate(st.body)
 
 
-print(f"Object is: {MY_OBJ}")
+print(f"Event is: {MY_EVENT}")
 while True:
     try:
-        expr = input("\nEnter an expression to evaluate obj: ")
+        expr = input("\nMatching expression: ")
     except KeyboardInterrupt:
         print()
         exit()
@@ -52,7 +52,10 @@ while True:
         continue
 
     try:
-        print(eval(expr, {}, MY_OBJ))
+        matched = eval(expr, {}, MY_EVENT)
     except Exception as ex:
         print(repr(ex))
+    else:
+        if matched:
+            print("Event matched!")
 
